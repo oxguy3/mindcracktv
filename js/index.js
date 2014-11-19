5a0f9eeeb60c767fe296bf42b98f956b9a72d2ff
Hayden Schiff <oxguy3@gmail.com>
2014-11-18 21:46:52 -0500

    committed everything so far

---
+++ js/index.js
@@ -0,0 +1,579 @@
+var shows = [
+
+  {
+    title: "SpeedRunners",
+    thumbnail: "img/game-speedrunners.png",
+    episodes: [
+
+      {
+        date: "2014-11-17T22:00:00",
+        views: [
+          {
+            yt: "SqBNIb2MCbA",
+            by: "supermcgamer"
+          },
+          {
+            yt: "iKfdp8b6O2o",
+            by: "avidyazen"
+          },
+          {
+            yt: "ZSM74-guyTk",
+            by: "coestar"
+          },
+          {
+            yt: "12a36MMZEtI",
+            by: "millbeeful"
+          }
+        ]
+      }
+
+    ]
+  },
+
+  {
+    title: "Space Engineers",
+    thumbnail: "img/game-spaceengineers.png",
+    episodes: [
+
+      {
+        date: "2014-11-17T10:00:00",
+        views: [
+          {
+            yt: "FgD-V6uFOlM",
+            by: "coestar"
+          },
+          {
+            yt: "R_hO-YZ6eD4",
+            by: "guudeboulderfist"
+          },
+          {
+            yt: "0rIxpabFros8",
+            by: "millbeeful"
+          }
+        ]
+      }
+
+    ]
+  },
+
+  {
+    title: "Mario Kart",
+    thumbnail: "img/game-mariokart8.png",
+    episodes: [
+
+      {
+        date: "2014-11-16T18:00:00",
+        views: [
+          {
+            yt: "oKKxxvFMkaM",
+            by: "coestar"
+          },
+          {
+            yt: "3pk-gs713q8",
+            by: "greatscottlp"
+          },
+          {
+            yt: "ibHpinmNSxw",
+            by: "supermcgamer"
+          },
+          {
+            yt: "RUfLWm31Hmk",
+            by: "pyropuncher"
+          },
+          {
+            yt: "plVcPRTY_H0",
+            by: "pauseunpause"
+          },
+          {
+            yt: "a4ldcr6m0Vc",
+            by: "millbeeful"
+          },
+          {
+            yt: "y5whP1UnHJw",
+            by: "guudeboulderfist"
+          }
+        ]
+      }
+
+    ]
+  },
+
+  {
+    title: "Trouble in Terrorist Town",
+    thumbnail: "img/game-ttt.png",
+    episodes: [
+
+      {
+        date: "2014-11-16T14:00:00",
+        views: [
+          {
+            yt: "tXpIT9elWBU",
+            by: "pauseunpause"
+          },
+          {
+            yt: "f-g_nsOQ_Yk",
+            by: "coestar"
+          },
+          {
+            yt: "bg7lgrjRmdQ",
+            by: "pyropuncher"
+          },
+          {
+            yt: "L-3RtH8EWEg",
+            by: "millbeeful"
+          },
+          {
+            yt: "c7jEIhd98Tg",
+            by: "weswilson"
+          }
+        ]
+      },
+
+      {
+        date: "2014-11-10T14:00:00",
+        views: [
+          {
+            yt: "-i_44ebDiS4",
+            by: "guudeboulderfist"
+          },
+          {
+            yt: "myEMG8WLoT4",
+            by: "pauseunpause"
+          },
+          {
+            yt: "tfBzB_tsmG4",
+            by: "coestar"
+          },
+          {
+            yt: "uh4geYqakAc",
+            by: "pyropuncher"
+          },
+          {
+            yt: "Et4UOl4mzSQ",
+            by: "millbeeful"
+          },
+          {
+            yt: "_wph0WaCKtM",
+            by: "parvplays"
+          }
+        ]
+      }
+
+    ]
+  },
+
+  {
+    title: "Prop Hunt",
+    thumbnail: "img/game-prophunt.png",
+    episodes: [
+
+      {
+        date: "2014-11-15T14:00:00",
+        views: [
+          {
+            yt: "4bDN84Clv9g",
+            by: "guudeboulderfist"
+          },
+          {
+            yt: "itI69ux707E",
+            by: "pauseunpause"
+          },
+          {
+            yt: "MVWd21j-KJ8",
+            by: "coestar"
+          },
+          {
+            yt: "gDqdVh78ab0",
+            by: "pyropuncher"
+          },
+          {
+            yt: "ppk6ust9eR0",
+            by: "millbeeful"
+          },
+          {
+            yt: "F3ZafAjCZDM",
+            by: "omgchad"
+          },
+          {
+            yt: "g_LGyfwV3ak",
+            by: "weswilson"
+          }
+        ]
+      }
+
+    ]
+  }
+
+];
+
+var mindcrackServerDays = [
+  {
+    day: "2014-11-17",
+    videos: [
+      {
+        date: "2014-11-17T14:04:00",
+        title: 'Minecraft Mindcrack Server Ep 40 - "PRANKED!!! Swedish Space Balls!!!"',
+        by: "generikb",
+        yt: "ZPlq_94KkOE"
+      },
+      {
+        date: "2014-11-17T12:00:00",
+        title: 'Gravy Rainbow (Mindcrack Season 5 Episode 5)',
+        by: "pauseunpause",
+        yt: "ZdlUN7GEMjw"
+      },
+      {
+        date: "2014-11-17T12:00:00",
+        title: 'Mindcrack #16: GRAVY RAINBOW!',
+        by: "omgchad",
+        yt: "pUd0tVYJeZE"
+      },
+      {
+        date: "2014-11-17T12:00:00",
+        title: 'Mindcrack: I\'m Thankful for... (Nov. 17)',
+        by: "aureylian",
+        yt: "KR92ozUit2s"
+      },
+      {
+        date: "2014-11-17T12:00:00",
+        title: 'Minecraft Mindcrack Survival 60 FPS Ep 26: All about that Base',
+        by: "blamethecontroller",
+        yt: "n1OWRPUPzaI"
+      },
+      {
+        date: "2014-11-17T12:00:00",
+        title: 'Minecraft - Mindcrack Season 5 - E20 - Back to Caving!',
+        by: "jsano19",
+        yt: "7yyImMCeUuc"
+      }
+    ]
+  }
+
+];
+
+var people = [
+  {
+    id: "adlingtont",
+    displayName: "Adlingtont",
+    displayNameShort: "Adlington"
+  },
+  {
+    id: "arkasmc",
+    displayName: "Arkas",
+    displayNameShort: "Arkas"
+  },
+  {
+    id: "aureylian",
+    displayName: "Aureylian",
+    displayNameShort: "Aureylian"
+  },
+  {
+    id: "avidyazen",
+    displayName: "AvidyaZEN",
+    displayNameShort: "Avidya"
+  },
+  {
+    id: "bdoubleo100",
+    displayName: "BdoubleO100",
+    displayNameShort: "BdoubleO"
+  },
+  {
+    id: "blamethecontroller",
+    displayName: "BlameTheController",
+    displayNameShort: "BlameTC"
+  },
+  {
+    id: "coestar",
+    displayName: "Coestar",
+    displayNameShort: "Coe"
+  },
+  {
+    id: "docm77",
+    displayName: "Docm77",
+    displayNameShort: "Docm"
+  },
+  {
+    id: "ethoslab",
+    displayName: "Etho",
+    displayNameShort: "Etho"
+  },
+  {
+    id: "generikb",
+    displayName: "Generikb",
+    displayNameShort: "Generikb"
+  },
+  {
+    id: "guudeboulderfist",
+    displayName: "GuudeBoulderfist",
+    displayNameShort: "Guude"
+  },
+  {
+    id: "imanderzel",
+    displayName: "AnderZEL",
+    displayNameShort: "AnderZEL"
+  },
+  {
+    id: "jsano19",
+    displayName: "JSano19",
+    displayNameShort: "JSano"
+  },
+  {
+    id: "kurtjmac",
+    displayName: "KurtJMac",
+    displayNameShort: "Kurt"
+  },
+  {
+    id: "mhykol",
+    displayName: "Mhykol",
+    displayNameShort: "Mhykol"
+  },
+  {
+    id: "millbeeful",
+    displayName: "Millbee",
+    displayNameShort: "Millbee"
+  },
+  {
+    id: "mindcracknetwork",
+    displayName: "Mindcrack Network",
+    displayNameShort: "Mindcrack"
+  },
+  {
+    id: "nebris88",
+    displayName: "Nebris",
+    displayNameShort: "Nebris"
+  },
+  {
+    id: "omgchad",
+    displayName: "OMGchad",
+    displayNameShort: "Chad"
+  },
+  {
+    id: "pakratt13",
+    displayName: "Pakratt13",
+    displayNameShort: "Pakratt"
+  },
+  {
+    id: "paulsoaresjr",
+    displayName: "PaulSoaresJr",
+    displayNameShort: "PaulSJr"
+  },
+  {
+    id: "pauseunpause",
+    displayName: "PauseUnpause",
+    displayNameShort: "Pause"
+  },
+  {
+    id: "pyropuncher",
+    displayName: "Pyropuncher",
+    displayNameShort: "Pyro"
+  },
+  {
+    id: "sethbling",
+    displayName: "SethBling",
+    displayNameShort: "SethBling"
+  },
+  {
+    id: "sevadus",
+    displayName: "Sevadus",
+    displayNameShort: "Sevadus"
+  },
+  {
+    id: "supermcgamer",
+    displayName: "SuperMCGamer",
+    displayNameShort: "MCGamer"
+  },
+  {
+    id: "thejims",
+    displayName: "thejims",
+    displayNameShort: "thejims"
+  },
+  {
+    id: "vechz",
+    displayName: "Vechs",
+    displayNameShort: "Vechs"
+  },
+  {
+    id: "vintagebeef",
+    displayName: "VintageBeef",
+    displayNameShort: "V. Beef"
+  },
+  {
+    id: "w92baj",
+    displayName: "W92Baj",
+    displayNameShort: "Baj"
+  },
+  {
+    id: "zisteau",
+    displayName: "Zisteau",
+    displayNameShort: "Zisteau"
+  },
+
+
+  {
+    id: "greatscottlp",
+    displayName: "GreatScottLP",
+    displayNameShort: "GreatScott"
+  },
+  {
+    id: "parvplays",
+    displayName: "YOGSCAST Parv",
+    displayNameShort: "Parv"
+  },
+  {
+    id: "weswilson",
+    displayName: "WesWilson",
+    displayNameShort: "Wes"
+  }
+];
+
+function getPersonById(query) {
+  for (var i = 0; i < people.length; i++) {
+    if (people[i].id == query) {
+      return people[i];
+    }
+  }
+  return null;
+}
+
+
+function generateShowBox(show) {
+  var box = $("#showTemplate").children().clone(true,true);
+
+  var latest = show.episodes[0];
+  var featuredIndex = Math.floor((Math.random() * latest.views.length));
+
+  box.find('[data-field="title-html"]').html(show.title);
+
+  box.find('[data-field="thumbnail-src"]').attr("src", show.thumbnail);
+
+  var latestDateStr = prettyDate(new Date(latest.date));
+  box.find('[data-field="datePretty-html"]').html(latestDateStr);
+
+  box.find('[data-field="date-livestamp"]').attr("data-livestamp", latest.date);
+
+  var featuredYoutubeLink = youtubeLink(latest.views[featuredIndex].yt);
+  box.find('[data-field="latest_featured_yt-href"]').attr("href", featuredYoutubeLink);
+
+  var featuredByShortStr = getPersonById(latest.views[featuredIndex].by).displayNameShort;
+  box.find('[data-field="latest_featured_by_short-html"]').html(featuredByShortStr);
+
+  var otherViews = "";
+  for (var i = 0; i < latest.views.length; i++) {
+    var view = latest.views[i];
+    var viewStr = "";
+    if (latest.views.length - 3 == i) viewStr += '<span class="nobr">';
+    viewStr += '<a href="';
+    viewStr += youtubeLink(view.yt);
+    viewStr += '" target="_blank" class="view-link-secondary" title="';
+    viewStr += getPersonById(view.by).displayName;
+    viewStr += '"><span class="view-link-secondary-icon playerpic-32 ';
+    viewStr += view.by;
+    viewStr += '"></span></a>';
+    otherViews += viewStr;
+  }
+  otherViews += "</span>";
+  box.find('[data-field="latest_all-html"]').html(otherViews);
+
+  return box;
+}
+
+
+function generateMindcrackToday() {
+  var listDiv = $(".mindcrack-today-list");
+  var today = mindcrackServerDays[0];
+
+  $(".mindcrack-today-datePretty").html(prettyDate(new Date(today.day)));
+
+  var listHtml = "";
+  for (var i = 0; i < today.videos.length; i++) {
+    var videoHtml = "";
+    var video = today.videos[i];
+    videoHtml += '<a href="';
+    videoHtml += youtubeLink(video.yt);
+    videoHtml += '" class="list-group-item mindcrack-today-list-item" data-imgsrc="';
+    videoHtml += youtubeHqThumbnail(video.yt);
+    videoHtml += '"><span class="pull-right mindcrack-today-playericon" title="';
+    videoHtml += getPersonById(video.by).displayName;
+    videoHtml += '"><span class="playerpic-24 ';
+    videoHtml += video.by;
+    videoHtml += '"></span></span>';
+    videoHtml += video.title;
+    videoHtml += '</a>';
+    listHtml += videoHtml;
+  }
+  listDiv.html(listHtml);
+}
+
+
+function generateContent() {
+
+  // generate Today on the Mindcrack Server
+  generateMindcrackToday();
+
+  var homeContent = $(".home-content");
+
+  // generate shows
+  for (var i = 0; i < shows.length; i++) {
+    homeContent.append(generateShowBox(shows[i]));
+
+    if ((i+1) % 3 == 0) {
+      homeContent.append('<div class="clearfix visible-lg-block"></div>');
+      homeContent.append('<div class="clearfix visible-md-block"></div>');
+    }
+    if ((i+1) % 2 == 0) {
+      homeContent.append('<div class="clearfix visible-sm-block"></div>');
+    }
+  }
+
+}
+
+generateContent();
+
+
+
+$(".home-content").tooltip({
+  placement: 'bottom',
+  selector: ".view-link-secondary, .show-box-btn-text",
+  trigger: 'hover'
+});
+
+
+
+$(".mindcrack-today-list").tooltip({
+  selector: '.mindcrack-today-playericon',
+  trigger: 'hover'
+});
+
+
+
+// showcase image handling
+var showcaseImgPlaceholder = "img/hqdefault-placeholder.jpg";
+$(".mindcrack-today-showcaseimg").attr("src", showcaseImgPlaceholder);
+
+$(".mindcrack-today-list-item").mouseover(function() {
+  var imgsrc = $(this).attr("data-imgsrc");
+  $(".mindcrack-today-showcaseimg").css("background-image", "url("+imgsrc+")");
+
+});
+
+$(".mindcrack-today-list").mouseout(function() {
+    $(".mindcrack-today-showcaseimg").css("background-image", "url("+showcaseImgPlaceholder+")");
+
+  }
+);
+
+
+
+
+
+function youtubeLink(yt) {
+  return "//youtu.be/" + yt;
+}
+
+function youtubeHqThumbnail(yt) {
+  return "//img.youtube.com/vi/"+yt+"/hqdefault.jpg";
+}
+
+function prettyDate(date) {
+  return date.toDateString();
+}
---
+++ run_win.bat
@@ -0,0 +1,3 @@
+cd %~dp0
+java -jar jlhttp.jar ./ 8080
+pause
